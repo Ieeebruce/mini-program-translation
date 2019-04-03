bc36811520c3d83e6e843efd183b26e0415b4789
ieeebruce <leihongtao1230@gmail.com>
2019-04-03 12:40:40 +0800

    完成基本功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+import {translate} from '../../utils/api.js'
 const app = getApp();
 Page({
 
@@ -7,9 +8,9 @@ Page({
    */
   data: {
     inputText: '',
-    result:'',
+    result: '',
     hideClearButton: true,
-    selectedLanguage: '',
+    selectedLanguage: {},
   },
   clearInputArea: function() {
     this.setData({
@@ -23,15 +24,34 @@ Page({
     })
     //键盘激活，显示清除按钮
     if (this.data.inputText.length > 0) {
-      this.setData({hideClearButton: false})
+      this.setData({
+        hideClearButton: false
+      })
     } else {
-      this.setData({hideClearButton: true})
+      this.setData({
+        hideClearButton: true
+      })
     }
   },
   onConfirm: function() {
     //调用翻译
-    if(this.data.inputText){
-      translate(this.data.inputText);
+    if (this.data.inputText) {
+      translate(this.data.inputText, {
+        from: 'auto',
+        to: app.globalData.selectedLanguage.lang
+      }).then(res => {
+        this.setData({
+          'result': res.trans_result[0].dst
+        });
+        console.log(this.data.result)
+        let history = wx.getStorageSync('history') || [];
+        history.unshift({
+          inputText: this.data.inputText,
+          result: res.trans_result[0].dst
+        });
+        history.length = history.length > 10 ? 10 : history.length;
+        wx.setStorageSync('history', history)
+      });
       this.setData({
         hideClearButton: false
       });
@@ -42,8 +62,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log("options")
-    console.log(options)
+    if (options.inputText) {
+      this.setData({ inputText: options.inputText })
+    }
   },
 
   /**
@@ -57,7 +78,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-    this.setData({'selectedLanguage': app.globalData.selectedLanguage.chs})
+    if (this.data.selectedLanguage !== app.globalData.selectedLanguage) {
+      this.setData({
+        'selectedLanguage': app.globalData.selectedLanguage.chs
+      });
+      this.onConfirm()
+      }
+    
   },
 
   /**
