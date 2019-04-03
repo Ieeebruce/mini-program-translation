ff31598472a333880c00fc88073f07f20c38ac1f
ieeebruce <leihongtao1230@gmail.com>
2019-04-03 10:16:04 +0800

    pages/index history
    基本样式布局和逻辑

--- pages/change/change.js
+++ pages/change/change.js
@@ -1,66 +1,71 @@
 // pages/change/change.js
+// const util = require('../../utils/util.js')
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  //页面的初始数据
   data: {
+    selectedLanguage: {},
+    languageList: app.globalData.languageList
+  },
+  chooseLanguage: function(e) {
+    let lang = e.currentTarget.dataset;
+    wx.setStorageSync('selectedLanguage', lang);
+    app.globalData.selectedLanguage = lang;
+    this.setData({'selectedLanguage' : lang});
+    wx.switchTab({
+      url: '/pages/index/index'
+    })
+  },
+
+  // 生命周期函数--监听页面加载
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  //生命周期函数--监听页面初次渲染完成
+
+  onReady: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+
+  //生命周期函数--监听页面显示
+
+  onShow: function() {
+    this.setData({selectedLanguage:app.globalData.selectedLanguage})
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+
+  //生命周期函数--监听页面隐藏
+  onHide: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  //生命周期函数--监听页面卸载
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,79 +1,97 @@
 // pages/index/index.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
     inputText: '',
+    result:'',
     hideClearButton: true,
-    language: '英语',
+    selectedLanguage: '',
   },
-  clearInputArea: function () {
-    this.setData({ inputText: '', hideClearButton: true })
+  clearInputArea: function() {
+    this.setData({
+      'inputText': '',
+      hideClearButton: true
+    })
   },
-  onInput: function () {
+  onInput: function(e) {
+    this.setData({
+      'inputText': e.detail.value
+    })
     //键盘激活，显示清除按钮
-    this.setData({ hideClearButton: false });
-
+    if (this.data.inputText.length > 0) {
+      this.setData({hideClearButton: false})
+    } else {
+      this.setData({hideClearButton: true})
+    }
   },
-  onConfirm: function () { 
+  onConfirm: function() {
     //调用翻译
-    this.setData({ hideClearButton: false });
+    if(this.data.inputText){
+      translate(this.data.inputText);
+      this.setData({
+        hideClearButton: false
+      });
+    }
+
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log("options")
+    console.log(options)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
+    this.setData({'selectedLanguage': app.globalData.selectedLanguage.chs})
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
